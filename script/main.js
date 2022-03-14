40b6b9208c77271189c4dde5848da86fa0257b24
Tim Davies <tim@eljetico.co.uk>
2022-03-14 17:12:25 +0000

    Adjust argument order

--- script/main.js
+++ script/main.js
@@ -156,7 +156,7 @@ var artificialHorizon = (function() {
 
   function drawActualHorizonPosition() {
     var cY = canvasStatic.height / 2;
-    var yPos = (pitchConstant / Math.sin(radians(_rawPitch))) - cY;
+    var yPos = cY - (pitchConstant / Math.sin(radians(_rawPitch)));
     updatePitchIndicator(yPos);
   }
 
