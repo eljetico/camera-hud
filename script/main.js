726fa577b7ac2777996e53e83cc06a194f5a3092
Tim Davies <tim@eljetico.co.uk>
2022-03-15 11:26:24 +0000

    Reverting calculation (doesn't like radians...)

--- script/main.js
+++ script/main.js
@@ -196,7 +196,7 @@ var artificialHorizon = (function() {
   }
 
   function drawFlatHorizonLine() {
-    var yPos = getHorizon(radians(pitch));
+    var yPos = getHorizon(pitch + 0 * Math.PI / 180);
 
     context.save();
     context.beginPath();
